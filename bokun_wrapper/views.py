2f8c5bc5984b518b5afa2624107a7e7615facd32
Hákon Þrastar Björnsson <hakon@reon.is>
2018-01-09 14:00:48 +0000

    lux/priv price tempfix|

--- bokun_wrapper/views.py
+++ bokun_wrapper/views.py
@@ -10,14 +10,14 @@ import json
 def get_private_price(count):
     if count < 5:
         return 19990
-    elif count < 15:
+    elif count <= 15:
         return 34990
 
 
 def get_luxury_price(count):
     if count < 4:
         return 27990
-    elif count < 8:
+    elif count <= 8:
         return 34990
 
 
